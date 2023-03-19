aec7f5bf1e31091c9bac85a3c9d78bcc7b8e26f3
hanbee <sonhanbi1002@gmail.com>
2023-03-19 21:17:13 +0900

    [feat] 검색 키워드 추가 실패와 블로그 검색을 async로 분리

--- src/main/java/com/task/searchblog/search/application/service/SearchCommandService.java
+++ src/main/java/com/task/searchblog/search/application/service/SearchCommandService.java
@@ -4,6 +4,7 @@ import com.task.searchblog.search.adapter.out.persistance.SearchRepository;
 import com.task.searchblog.search.domain.Search;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SearchCommandService {
     private final SearchRepository searchRepository;
 
+    @Async
     public void addSearchKeyword(Long memberId, String keyword) {
         searchRepository.save(Search.create(memberId, keyword));
     }
