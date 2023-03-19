538e63100756838d3c1bd5d3fc65970352ed7d22
hanbee <sonhanbi1002@gmail.com>
2023-03-19 21:00:15 +0900

    [feat] 에러 관련 추가 처리

---
+++ src/main/java/com/task/searchblog/common/constant/ValidatorCodeConstants.java
@@ -0,0 +1,9 @@
+package com.task.searchblog.common.constant;
+
+public class ValidatorCodeConstants {
+    public static final String NOT_EMPTY = "NotEmpty";
+    public static final String NOT_BLANK = "NotBlank";
+    public static final String MIN = "Min";
+    public static final String MAX = "Max";
+    public static final String TYPE_MISMATCH = "typeMismatch";
+}
--- src/main/java/com/task/searchblog/common/exception/handler/CommonExceptionAdvice.java
+++ src/main/java/com/task/searchblog/common/exception/handler/CommonExceptionAdvice.java
@@ -1,12 +1,21 @@
 package com.task.searchblog.common.exception.handler;
 
 import com.task.searchblog.common.exception.BusinessException;
+import com.task.searchblog.common.exception.ExceptionType;
 import com.task.searchblog.common.exception.ExternalClientCanNotProceedException;
 import com.task.searchblog.common.model.CommonErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+import static com.task.searchblog.common.constant.ValidatorCodeConstants.*;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,5 +30,34 @@ public class CommonExceptionAdvice {
     protected ResponseEntity<CommonErrorResponse> handleException(BusinessException exception) {
         log.error("Business Exception occurred.");
         return ResponseEntity.status(exception.getStatus()).body(CommonErrorResponse.of(exception));
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    protected ResponseEntity<CommonErrorResponse> handleException(final Exception e) {
+        if (e instanceof BindException) {
+            BindException bindException = (BindException) e;
+            String message = handleBindException(bindException);
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                    .body(CommonErrorResponse.of(new BusinessException(ExceptionType.INVALID_REQUEST, message)));
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(CommonErrorResponse.of(new BusinessException(ExceptionType.UNEXPECTED, e.getMessage())));
+        }
+    }
+
+    private String handleBindException(final BindException exception) {
+        List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
+
+        for (FieldError error : fieldErrorList) {
+            if (NOT_EMPTY.equals(error.getCode()) || NOT_BLANK.equals(error.getCode())) {
+                return error.getField() + "은(는) " + error.getDefaultMessage();
+            } else if (MIN.equals(error.getCode()) || MAX.equals(error.getCode())) {
+                return error.getField() + "은(는)" + error.getDefaultMessage();
+            } else if (TYPE_MISMATCH.equals(error.getCode())) {
+                return error.getRejectedValue() + "은(는) " + error.getField() + " 타입에 존재하지 않는 값입니다.";
+            }
+        }
+
+        return Strings.EMPTY;
     }
 }
